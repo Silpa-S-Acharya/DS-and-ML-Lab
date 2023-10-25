c31c8b7590a76ca74a9f867c3422ce53bd9ee480
Acharya Silpa <117834398+Silpa-S-Acharya@users.noreply.github.com>
2023-10-25 22:44:04 +0530

    Create pgm3.py
    4 programs

---
+++ pgm3.py
@@ -0,0 +1,44 @@
+1. 
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.arrange(0,3*np.pi,0.1)
+y=np.sin(x)
+plt.plot(x,y)
+plt.show()
+
+2.
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.arrange(0,3*np.pi,0.1)
+y_sin=np.sin(x)
+y_cos=np.cos(x)
+plt.plot(x,y_sin)
+plt.plot(x,y_cos)
+plt.xlabel('xaxis label')
+plt.ylabel('yaxis label')
+plt.title('Sine and Cosine')
+plt.title(['Sine and Cosine'])
+plt.show()
+
+3.
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.arrange(0,3*np.pi,0.1)
+y_sin=np.sin(x)
+y_cos=np.cos(x)
+plt.subplot(2,1,1)
+plt.plot(x,y_sin)
+plt.title('Sine')
+plt.subplot(2,1,1)
+plt.title('Cosine')
+plt.show()
+
+4.
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+tstimg=img.imread('Cycle3/cat.jpeg)
+plt.subplot(1,2,1)
+plt.imshow(tstimg)
+plt.subplot(1,2,2)
+plt.imshow(tstimg)   
