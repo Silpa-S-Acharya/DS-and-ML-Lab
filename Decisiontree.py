b920911ea07267939c39948129ad922896d87580
Acharya Silpa <117834398+Silpa-S-Acharya@users.noreply.github.com>
2023-11-24 15:25:12 +0530

    Update Decisiontree.py

--- Decisiontree.py
+++ Decisiontree.py
@@ -6,28 +6,28 @@ from sklearn import tree
 
 import matplotlib.pyplot as plt
 
-from sklearn model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
-from sklearn tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
 
-iris-load_iris()
+iris=load_iris()
 
 x=iris.data
 
 y=iris.target
 
-x_train_x_test_y_train_y_test = train_test_split(x,y,test_size=0.3.random_state=1)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3,random_state=1)
 
-clf-DecisionTreeClassifier()
+clf=DecisionTreeClassifier()
 
-clf-clf fit(x_train_y_train)
+clf=clf.fit(x_train,y_train)
 
-y_pred-clf.predict(x_test)
+y_pred=clf.predict(x_test)
 
-print("Accuracy: ", metrics.accuracy_score(y_test_y_pred))
+print("Accuracy: ", metrics.accuracy_score(y_test,y_pred))
 
 plt.figure(figsize=(15.15))
 
-tree.plot_tree(clf fontsize=10,filled-True.rounded-True.class_names-iris.target_names.fa ture_names-iris.feature_names)
+tree.plot_tree(clf,fontsize=10,filled=True,rounded=True,class_names=iris.target_names,feature_names=iris.feature_names)
 
-plt.show
+plt.show()
