8389a55670dab2bd04a8707e6913025ef54c49ed
Acharya Silpa <117834398+Silpa-S-Acharya@users.noreply.github.com>
2023-11-24 14:25:36 +0530

    Update Svm.py

--- Svm.py
+++ Svm.py
@@ -10,13 +10,13 @@ from sklearn import svm
 
 from sklearn import metrics
 
-cancer datasets.load_breast_cancer()
+cancer=datasets.load_breast_cancer()
 
-x_trainx_test.y_trainy_test-train_test_split(cancer.data, cancer target, test_size=-0.3, random_state=109)
+x_train,x_test,y_train,y_test=train_test_split(cancer.data, cancer.target, test_size=0.3, random_state=109)
 
 clf=svm.SVC(kernel='linear')
 
-clf fit (x_train.y_train)
+clf.fit(x_train,y_train)
 
 y_pred=clf.predict(x_test)
 
@@ -24,8 +24,8 @@ print("actual values",y_test)
 
 print("predicted values",y_pred)
 
-print("accuracy" metrics.accuracy_score(y_test.y_pred))
+print("accuracy" ,metrics.accuracy_score(y_test,y_pred))
 
-print("precision" metrics.precision_score(y_test,y_pred))
+print("precision" ,metrics.precision_score(y_test,y_pred))
 
 print("recall",metrics.recall_score(y_test,y_pred))
